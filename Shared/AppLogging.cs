402bdde087d5f34218866ca805030cf2382de022
Tinus Janse van Rensburg <tinus.janse-van-rensburg@hensoldt.net>
2025-05-02 12:47:32 +0200

    Shared Project
    - Logger configured
    - Nugget packages updated correspondingly

---
+++ Shared/AppLogging.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Shared;
+
+// Logger class
+public class AppLogging
+{
+    public static void Initialise()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+            .CreateLogger();
+    }
+}
---
+++ Shared/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Shared;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSharedKernel(this IServiceCollection services)
+    {
+        // use Serilog as the single provider
+        services.AddLogging(builder =>
+        {
+            builder.ClearProviders();
+            builder.AddSerilog(Log.Logger, dispose: true);
+        });
+
+        // we can in the future add more services here, currently we'll only make use of the logger
+        return services;
+    }
+}
