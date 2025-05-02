336e1afc5447c6051c5f6d36eb09e151087f5b21
Tinus Janse van Rensburg <tinus.janse-van-rensburg@hensoldt.net>
2025-05-02 14:01:28 +0200

    EventBridge DB Context added

---
+++ EventBridge/EventDb.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Messages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventBridge;
+
+public class EventDb : DbContext
+{
+    public DbSet<PatientEvent> PatientEvents => Set<PatientEvent>();
+
+    // we should update this to make it configurable in the future
+    protected override void OnConfiguring(DbContextOptionsBuilder b)
+    {
+        b.UseNpgsql("Host=localhost;Port=5433;Database=postgres;Username=postgres;Password=dev");
+    }
+
+}
