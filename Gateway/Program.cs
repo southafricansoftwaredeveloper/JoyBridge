6f3ce1adf0be06719af6bd86cd716b8f4c6815ab
Tinus Janse van Rensburg <tinus.janse-van-rensburg@hensoldt.net>
2025-05-02 14:43:20 +0200

    Gateway functional
    - Publishing of events working
    - EventBridge persistence working
    - Lots of closed WebSocketExceptions occuring

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,5 +1,43 @@
+using Gateway.Internal;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Serilog;
+using Shared;
+
+AppLogging.Initialise();
+
 var builder = WebApplication.CreateBuilder(args);
+
+// use serilog
+builder.Host.UseSerilog();
+
+// we'll use our logger service from the Shared project
+builder.Services.AddSharedKernel();
+
+builder.WebHost.ConfigureKestrel(opts =>
+{
+    // port 5130 and plain http for our web socket connections, this needs to be secured going forward
+    opts.ListenLocalhost(5130, lo => lo.Protocols = HttpProtocols.Http1);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Enable WebSockets for data-plane
+app.UseWebSockets();
+
+// Map WebSocket endpoint
+app.Map("/ws", async ctx =>
+{
+    if (!ctx.WebSockets.IsWebSocketRequest)
+    {
+        ctx.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return;
+    }
+
+    using var ws = await ctx.WebSockets.AcceptWebSocketAsync();
+    
+    Log.Information("WS connected from {Client}", ctx.Connection.RemoteIpAddress);
+    
+    await WebSocketEcho.Run(ws, ctx.RequestAborted);
+});
+
 app.Run();
