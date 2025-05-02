1897804b0db2959aee0fdb3dc3b1865a22dc1439
Tinus Janse van Rensburg <tinus.janse-van-rensburg@hensoldt.net>
2025-05-02 12:36:11 +0200

    Added PatientEvent record

---
+++ Contracts/Messages/PatientEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contracts.Messages;
+
+public record PatientEvent
+{
+    [Key]
+    public long Id { get; set; }
+    public string  PatientId  { get; init; } = default!;
+    public string  DeviceId   { get; init; } = default!;
+    public DateTimeOffset Timestamp { get; init; }
+    public double  HeartRate  { get; init; }
+    public double  SpO2       { get; init; }
+}
