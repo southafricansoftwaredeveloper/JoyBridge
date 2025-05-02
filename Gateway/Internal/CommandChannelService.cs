a3855bcd19641cd94225607a6664b97ca906a481
Tinus Janse van Rensburg <tinus.janse-van-rensburg@hensoldt.net>
2025-05-02 15:28:55 +0200

    Added CommandChannelService to Gateway

---
+++ Gateway/Internal/CommandChannelService.cs
@@ -0,0 +1,33 @@
+﻿using Control;
+using Grpc.Core;
+
+namespace Gateway.Internal;
+
+public class CommandChannelService : CommandChannel.CommandChannelBase
+{
+    private readonly ILogger<CommandChannelService> _log;
+    
+    private readonly CommandChannel.CommandChannelClient _agent;
+
+    public CommandChannelService(ILogger<CommandChannelService> log, CommandChannel.CommandChannelClient agent)
+    {
+        _log = log;
+        _agent = agent;
+    }
+
+    public override async Task<Ack> PauseStream(PauseRequest req, ServerCallContext ctx)
+    {
+        _log.LogInformation("Gateway forwarding PauseStream…");
+        var ack = await _agent.PauseStreamAsync(req);
+        _log.LogInformation("Forward succeeded: {Msg}", ack.Message);
+        return ack;
+    }
+    
+    public override async Task<Ack> ResumeStream(ResumeRequest req, ServerCallContext ctx)
+    {
+        _log.LogInformation("Gateway forwarding ResumeStream: {Reason}", req.Reason);
+        var ack = await _agent.ResumeStreamAsync(req);
+        _log.LogInformation("Forward succeeded: {Msg}", ack.Message);
+        return ack;
+    }
+}
--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,4 +1,6 @@
+using Control;
 using Gateway.Internal;
+using Grpc.Net.Client;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Serilog;
 using Shared;
@@ -13,6 +15,21 @@ builder.Host.UseSerilog();
 // we'll use our logger service from the Shared project
 builder.Services.AddSharedKernel();
 
+builder.Services.AddSingleton(sp =>
+{
+    // polling agent will communicate over port 6001, we need to configure this going forward
+    var channel = GrpcChannel.ForAddress("http://localhost:6001",new GrpcChannelOptions
+    {
+        HttpHandler = new SocketsHttpHandler
+        {
+            EnableMultipleHttp2Connections = true
+        }
+    });
+    return new CommandChannel.CommandChannelClient(channel);
+});
+
+builder.Services.AddGrpc();
+
 builder.WebHost.ConfigureKestrel(opts =>
 {
     // port 5130 and plain http for our web socket connections, this needs to be secured going forward
@@ -23,6 +40,9 @@ var app = builder.Build();
 
 // Enable WebSockets for data-plane
 app.UseWebSockets();
+
+// map the gRPC
+app.MapGrpcService<CommandChannelService>();
 
 // Map WebSocket endpoint
 app.Map("/ws", async ctx =>
